f030a4ed1a59cb21059b0f8cfefd60381d2afcdc
Pongelupe <pongelupe1998@gmail.com>
2017-09-20 17:53:41 -0300

    finished the response to /Cadastro/cadastrarPaciente post request

--- naasp/src/main/resources/br/com/naasp/resources/Cadastro.java
+++ naasp/src/main/resources/br/com/naasp/resources/Cadastro.java
@@ -1,5 +1,22 @@
 package br.com.naasp.resources;
 
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import br.com.naasp.resources.model.responses.CadastroPacienteResposta;
+
+@RequestMapping(value = "/Cadastro")
 public class Cadastro {
+
+	@RequestMapping(value = "/cadastroPaciente", method = RequestMethod.POST, produces = "application/json", consumes = "application/json")
+	public String adicionaPaciente(@RequestBody String paciente) {
+		boolean erroFlag = true;
+
+		CadastroPacienteResposta responseObject = new CadastroPacienteResposta(erroFlag);
+		responseObject.setIdPaciente("312wdqe");
+		String response = responseObject.toJson().toString();
+		return response;
+	}
 
 }
