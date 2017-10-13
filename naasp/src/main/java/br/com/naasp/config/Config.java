cfb117f9cf985a52eb1e1df2b135545606bf51cb
Pongelupe <pongelupe1998@gmail.com>
2017-10-13 19:37:31 -0300

    fixed some cors issues

--- naasp/src/main/java/br/com/naasp/config/Config.java
+++ naasp/src/main/java/br/com/naasp/config/Config.java
@@ -25,7 +25,7 @@ public class Config {
 		return new WebMvcConfigurerAdapter() {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**").allowedOrigins("http://localhost:3000");
+				registry.addMapping("/**");
 			}
 		};
 	}
