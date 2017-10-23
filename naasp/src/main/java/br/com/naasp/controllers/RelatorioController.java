249a8b7707c432394a9a9abdcf5dc152b5388cde
Pongelupe <pongelupe1998@gmail.com>
2017-10-22 22:59:47 -0200

    finished developing /gerarRelatorio without params

--- naasp/src/main/java/br/com/naasp/controllers/RelatorioController.java
+++ naasp/src/main/java/br/com/naasp/controllers/RelatorioController.java
@@ -5,9 +5,13 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.sql.SQLException;
+
+import javax.sql.DataSource;
 
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ByteArrayResource;
 import org.springframework.core.io.Resource;
 import org.springframework.http.MediaType;
@@ -18,9 +22,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import br.com.naasp.models.Relatorio;
+import net.sf.jasperreports.engine.JRException;
 
 @Controller
 public class RelatorioController {
+
+	@Autowired
+	private DataSource dbsource;
 
 	@RequestMapping(value = "gerarRelatorio", method = org.springframework.web.bind.annotation.RequestMethod.POST)
 	public @ResponseBody ResponseEntity<Resource> gerarRelatorio(@RequestBody String json) {
@@ -29,16 +37,15 @@ public class RelatorioController {
 		try {
 
 			relatorio = new Relatorio(new JSONObject(json));
-			File relatorioPDF = relatorio.toFile();
+			File relatorioPDF = relatorio.toFile(dbsource);
 			Path path = Paths.get(relatorioPDF.getAbsolutePath());
 			ByteArrayResource resource = new ByteArrayResource(Files.readAllBytes(path));
 
 			return ResponseEntity.ok().contentLength(relatorioPDF.length())
 					.contentType(MediaType.parseMediaType("application/octet-stream")).body(resource);
-		} catch (JSONException | IOException e) {
+		} catch (JSONException | IOException | JRException | SQLException e) {
 			return null;
 		}
-
 	}
 
 }
--- naasp/src/main/java/br/com/naasp/models/Relatorio.java
+++ naasp/src/main/java/br/com/naasp/models/Relatorio.java
@@ -1,11 +1,26 @@
 package br.com.naasp.models;
 
 import java.io.File;
+import java.sql.SQLException;
+import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.sql.DataSource;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import net.sf.jasperreports.engine.JRException;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.engine.export.JRPdfExporter;
+import net.sf.jasperreports.export.SimpleExporterInput;
+import net.sf.jasperreports.export.SimpleOutputStreamExporterOutput;
 
 public class Relatorio {
 
@@ -15,6 +30,10 @@ public class Relatorio {
 	private static class RelatorioKeys {
 
 		private static final String PARAMETROS = "parametros";
+		private static final String JASPER_FILE_NAME = "Simple_Blue.jasper";
+		// private static final String JASPER_FILE_PATH = System.getProperty("user.dir")
+		// + "/src/main/resources/";
+		private static final String JASPER_FILE_PATH = "src/main/resources/";
 	}
 
 	private void setParametros(JSONArray jsonArray) throws JSONException {
@@ -26,13 +45,34 @@ public class Relatorio {
 		return nome;
 	}
 
-	public File toFile() {
-		return null;
+	public File toFile(DataSource dataSource) throws JRException, SQLException {
+		Map<String, Object> params = new HashMap<String, Object>();
+		JasperPrint jasperPrinted = JasperFillManager.fillReport(
+				RelatorioKeys.JASPER_FILE_PATH + RelatorioKeys.JASPER_FILE_NAME, params, dataSource.getConnection());
+		JRPdfExporter exporter = new JRPdfExporter();
+
+		exporter.setExporterInput(new SimpleExporterInput(jasperPrinted));
+		File pdf = new File(RelatorioKeys.JASPER_FILE_PATH + nome);
+		exporter.setExporterOutput(new SimpleOutputStreamExporterOutput(pdf));
+
+		exporter.exportReport();
+
+		return pdf;
 	}
 
 	public Relatorio(JSONObject json) throws JSONException {
+		cleanDir();
+
 		if (json.has(RelatorioKeys.PARAMETROS))
 			setParametros(json.getJSONArray(RelatorioKeys.PARAMETROS));
+
+		this.nome = LocalDate.now() + ".pdf";
+	}
+
+	private void cleanDir() {
+		File folder = new File(RelatorioKeys.JASPER_FILE_PATH);
+		List<File> files = Arrays.asList(folder.listFiles());
+		files.stream().filter(f -> f.getName().endsWith(".pdf")).forEach(f -> f.delete());
 	}
 
 }
